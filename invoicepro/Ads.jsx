a51f58affa87362497f4cc90daa77a5de0f814a0
pk923 <pk2372264@gmail.com>
2025-12-24 23:48:56 -0800

    Update Ads.jsx

--- invoicepro/Ads.jsx
+++ invoicepro/Ads.jsx
@@ -1,46 +1,33 @@
-import { useEffect } from "react";
+useEffect(() => {
+  const scripts = [];
 
-export default function Ads() {
-  useEffect(() => {
-    // Ad script 1
-    const s1 = document.createElement("script");
-    s1.src = "https://pl28330114.effectivegatecpm.com/ac/a4/09/aca409517e820f6ead8e0743c70d18be.js";
-    s1.async = true;
-    document.body.appendChild(s1);
+  const addScript = (src, attrs = {}) => {
+    const s = document.createElement("script");
+    s.src = src;
+    s.async = true;
+    Object.entries(attrs).forEach(([k, v]) => s.setAttribute(k, v));
+    document.body.appendChild(s);
+    scripts.push(s);
+  };
 
-    // Ad script 2
-    const s2 = document.createElement("script");
-    s2.src = "https://pl28330150.effectivegatecpm.com/13/75/83/1375836df6e7980ce49d73c6afc6d733.js";
-    s2.async = true;
-    document.body.appendChild(s2);
+  addScript("https://pl28330114.effectivegatecpm.com/ac/a4/09/aca409517e820f6ead8e0743c70d18be.js");
+  addScript("https://pl28330150.effectivegatecpm.com/13/75/83/1375836df6e7980ce49d73c6afc6d733.js");
 
-    // Ad script 3 (iframe)
-    window.atOptions = {
-      key: "772831fe045efd4b47c2d839addf68e2",
-      format: "iframe",
-      height: 300,
-      width: 160,
-      params: {}
-    };
+  window.atOptions = {
+    key: "772831fe045efd4b47c2d839addf68e2",
+    format: "iframe",
+    height: 300,
+    width: 160,
+    params: {}
+  };
 
-    const s3 = document.createElement("script");
-    s3.src = "https://www.highperformanceformat.com/772831fe045efd4b47c2d839addf68e2/invoke.js";
-    s3.async = true;
-    document.body.appendChild(s3);
-
-    // Ad script 4
-    const s4 = document.createElement("script");
-    s4.src = "https://pl28330167.effectivegatecpm.com/66c012a1833046421187ce1b10f6f8c1/invoke.js";
-    s4.async = true;
-    s4.setAttribute("data-cfasync", "false");
-    document.body.appendChild(s4);
-  }, []);
-
-  // Ad container (5th)
-  return (
-    <div
-      id="container-66c012a1833046421187ce1b10f6f8c1"
-      style={{ margin: "20px 0", textAlign: "center" }}
-    />
+  addScript("https://www.highperformanceformat.com/772831fe045efd4b47c2d839addf68e2/invoke.js");
+  addScript(
+    "https://pl28330167.effectivegatecpm.com/66c012a1833046421187ce1b10f6f8c1/invoke.js",
+    { "data-cfasync": "false" }
   );
-}
+
+  return () => {
+    scripts.forEach(s => document.body.removeChild(s));
+  };
+}, []);
